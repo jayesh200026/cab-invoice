03c0f3c2c25a0dde8e9e75cccb265ae2bfcc22d7
jayesh200026 <jayeshkumar8660@gmail.com>
2021-09-29 19:13:30 +0530

    Added featue to compute fare for multiple rides

--- src/test/java/testcases/CabInvoiceTest.java
+++ src/test/java/testcases/CabInvoiceTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.invoice.CabInvoiceGenerator;
+import com.invoice.Ride;
 
 public class CabInvoiceTest {
 
@@ -17,6 +18,16 @@ public class CabInvoiceTest {
 	public void givenJourney_withMiniMumFare_returnsTrue() {
 		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
 		Assert.assertEquals(5, cabInvoiceGenerator.generateFare(0.2, 2.5), 0.001);
+
+	}
+
+	@Test
+	public void givenMultipleRide_matchingWithExpectedFare_returnsTrue() {
+		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+		Ride[] rides = { new Ride(5, 10), new Ride(10, 25) };
+		double expectedTotalFare = 185;
+		double epsilon = 1e-15;
+		Assert.assertEquals(expectedTotalFare, cabInvoiceGenerator.generateTotalFare(rides), epsilon);
 
 	}
 }
